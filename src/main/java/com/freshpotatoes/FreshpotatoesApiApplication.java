71046b93db6cf6dcf7c3757a1fa17fc5badb9f36
Matt Fritz <mfritzer@gmail.com>
2017-03-13 09:24:13 -0600

    protected static final InitialCommitFactoryImpl

---
+++ src/main/java/com/freshpotatoes/FreshpotatoesApiApplication.java
@@ -0,0 +1,12 @@
+package com.freshpotatoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshpotatoesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshpotatoesApiApplication.class, args);
+	}
+}
