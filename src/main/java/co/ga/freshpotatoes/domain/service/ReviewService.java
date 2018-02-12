2121fbe8d248772a16eafe5c5fc44ab7650cc6be
thequinto <quinto.timothy@gmail.com>
2018-02-12 04:15:04 -0500

    return films based on reviews and timeframe main() - find some film IDs that had good reviews

--- src/main/java/co/ga/freshpotatoes/domain/service/ReviewService.java
+++ src/main/java/co/ga/freshpotatoes/domain/service/ReviewService.java
@@ -1,37 +1,105 @@
 package co.ga.freshpotatoes.domain.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import co.ga.freshpotatoes.domain.entity.Film;
 import co.ga.freshpotatoes.domain.entity.ReviewResponse;
 
 @Service
 public class ReviewService {
+    private static final Logger log = LoggerFactory.getLogger(ReviewService.class);
     private static String URI = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?";
     private static String FIELD1 = "films=";
     
-    public List<ReviewResponse> getReviews(String filmIds, boolean useFilter) {
-        RestTemplate template = new RestTemplate();
-        ReviewResponse[] responses = template.getForObject(URI+FIELD1+filmIds, ReviewResponse[].class);
-        List<ReviewResponse> responsesList = new ArrayList<>();
+    public List<Film> filterFilmsByReviews(List<Film> films, int minReviews, float minRating) {
+        List<ReviewResponse> responses = getReviews(films, minReviews, minRating);
+        Set<Integer> filteredFilmIds = new HashSet<>();
         for (ReviewResponse r : responses) {
-            if (!useFilter || (r.getReviews().size() >= 5 && r.getAvgRating() >= 4.0))
-                responsesList.add(r);
+            filteredFilmIds.add((int)r.getFilmId());
         }
+        List<Film> filteredFilms = new LinkedList<>(films);
+        for (Film f : films) {
+            if (filteredFilmIds.contains((int)f.getId())) {
+                filteredFilms.add(f);
+            }
+        }
+        return filteredFilms;
+    }
+    
+    private List<ReviewResponse> getReviews(List<Film> films, int minReviews, float minRating) {
+        List<ReviewResponse> responsesList = new ArrayList<>();
+
+        RestTemplate template = new RestTemplate();
+        int BATCH_SIZE = 100;
+        int min = 0;
+        int max = BATCH_SIZE;
+        do {
+            StringBuilder s = new StringBuilder();
+            for (int i = min; i < max; i++) {
+                if (i == films.size()) break;
+                s.append(films.get(i).getId());
+                if (i < max - 1 && i < films.size() - 1) s.append(",");
+            }
+            log.info("going to reviews API: " + URI+FIELD1+s.toString());
+            ReviewResponse[] responses = template.getForObject(URI+FIELD1+s.toString(), ReviewResponse[].class);
+            for (ReviewResponse r : responses) {
+                if (r.getReviews().size() >= minReviews && r.getAvgRating() >= minRating)
+                    responsesList.add(r);
+            }
+            min += BATCH_SIZE;
+            max += BATCH_SIZE;
+        } while (max < films.size() + BATCH_SIZE);
+        
         return responsesList;
     }
     
+    private List<ReviewResponse> getReviewsByFilmIds(String filmIds) {
+        List<ReviewResponse> responsesList = new ArrayList<>();
+    
+        RestTemplate template = new RestTemplate();
+        String[] tokens = filmIds.split(",");
+        int BATCH_SIZE = 100;
+        int min = 0;
+        int max = BATCH_SIZE;
+        do {
+            StringBuilder s = new StringBuilder();
+            for (int i = min; i < max; i++) {
+                if (i == tokens.length) break;
+                s.append(tokens[i]);
+                if (i < max - 1 && i < tokens.length - 1) s.append(",");
+            }
+            log.info("going to reviews API: " + URI+FIELD1+s.toString());
+            ReviewResponse[] responses = template.getForObject(URI+FIELD1+s.toString(), ReviewResponse[].class);
+            for (ReviewResponse r : responses) {
+                if (r.getReviews().size() >= 5 && r.getAvgRating() >= 4.0)
+                    responsesList.add(r);
+            }
+            min += BATCH_SIZE;
+            max += BATCH_SIZE;
+        } while (max < tokens.length + BATCH_SIZE);
+        
+        return responsesList;
+    }
+
     public static void main(String[] args) {
         ReviewService s = new ReviewService();
         StringBuilder filmIds = new StringBuilder();
-        for (int i = 1; i <= 100; i++) {
+        int min = 7000;
+        int max = 8000;
+        for (int i = min; i <= max; i++) {
             filmIds.append(i);
-            if (i < 100) filmIds.append(",");
+            if (i < max - 1) filmIds.append(",");
         }
-        List<ReviewResponse> r = s.getReviews(filmIds.toString(), true);
-        for (ReviewResponse q : r) System.out.println(q);
+        List<ReviewResponse> r = s.getReviewsByFilmIds("7459,7535,7554,7566,7574,7579,7580,7611,7634,7635,7641,7674,7675,7684,7713,7719,7732,7740,7783,7807,7810,7821,7830,7846,7850,7912,7972,7990,8006,8013,8014,8015,8041,8048,8068,8070,8086,8088,8110,8128,8131,8133,8147,8168,8172,8193,8194,8227,8237,8263,8266,8308,8318,8320,8322,8334,8368,8370,8380,8389,8398,8430,8461,8467,8476,8496,8543,8548,8578,8614,8623,8639,8673,8743,8747,8748,8755,8756,8782,8826,8842,8843,8854,8904,8924,8937,8957,8986,8990,8993,9005,9027,9087,9120,9139,9191,9239,9276,9278,9291,9305,9308,9328,9332,9354,9404,9419,9421,9448,9456,9466,9508,9586,9589,9590,9616,9630,9642,9690,9712,9714,9715,9788,9790,9818,9822,9857,9869,9926,9948,9965,9969,9971,9982,9986,9994,9997,10040,10060,10084,10090,10099,10109,10129,10217,10247,10250,10273,10287");
+        for (ReviewResponse q : r) System.out.println(q.getFilmId());
     }
 }
--- src/main/java/co/ga/freshpotatoes/web/FilmsController.java
+++ src/main/java/co/ga/freshpotatoes/web/FilmsController.java
@@ -1,29 +1,72 @@
 package co.ga.freshpotatoes.web;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import co.ga.freshpotatoes.domain.entity.Film;
 import co.ga.freshpotatoes.domain.entity.Genre;
 import co.ga.freshpotatoes.domain.repository.FilmRepository;
 import co.ga.freshpotatoes.domain.repository.GenreRepository;
+import co.ga.freshpotatoes.domain.service.ReviewService;
 
 @RestController
 public class FilmsController {
+  private static final Logger log = LoggerFactory.getLogger(FilmsController.class);
   @Autowired GenreRepository genreRepository;
   @Autowired FilmRepository filmRepository;
+  @Autowired ReviewService reviewService;
 
   private static final String template = "id=%s, offset=%s, limit=%s\n";
 
   @RequestMapping(value="/films/{film_id}/recommendations", method=RequestMethod.GET)
-  public Set<Film> recommendations(@PathVariable Long film_id) {
-      return new java.util.LinkedHashSet<Film>();
+  public Set<Film> recommendations(@PathVariable Long film_id) throws ParseException {
+      return getRecommendationsWithinYears(film_id, 7.5);
+  }
+  
+  private Set<Film> getRecommendationsWithinYears(Long film_id, double numYears) throws ParseException {
+      Film thisFilm = filmRepository.findOne(film_id);
+      log.info("searching for genre of: " + film_id);
+      Genre genre = thisFilm.getGenre();
+      log.info("searching for films of genre: " + genre);
+      List<Film> filmsByGenre = filmRepository.findByGenre(genre);
+      // get from this genre films that have 5+ reviews averaging 4.0+
+      List<Film> films = reviewService.filterFilmsByReviews(filmsByGenre, 5, 4.0f);
+      StringBuilder s = new StringBuilder();
+      for (Film f : films)
+          s.append(f.getId() + " ");
+      log.info("found in these highly rated films in the same genre: " + s);
+      // remove films outside 15 years
+      double RANGE = numYears*365.25*24*60*60*1000;
+      DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+      Date thisDate = df.parse(thisFilm.getReleaseDate());
+      for (Film f : films) {
+          Date releaseDate = df.parse(f.getReleaseDate());
+          if (Math.abs(thisDate.getTime() - releaseDate.getTime()) > RANGE) {
+              films.remove(f);
+          }
+      }
+      log.info("removed films outside of +-" + RANGE + " ms");
+      // remove the originally requested film
+      films.remove(thisFilm);
+      // finally sort by film ID
+      films.sort(new java.util.Comparator<Film>() {
+        @Override
+        public int compare(Film o1, Film o2) {
+            return (int)(o1.getId() - o2.getId());
+        }
+      });
+      return new java.util.LinkedHashSet<Film>(films);
   }
 }
